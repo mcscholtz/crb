3c9a8e0e05080a4df5f3f582a3aebc2ee1f45639
Marius <mc.scholtz@gmail.com>
2018-09-01 12:01:15 -0700

    Added tests

--- tests/rb-dequeue-test.c
+++ tests/rb-dequeue-test.c
@@ -9,14 +9,20 @@ void rb_create_test(CuTest * tc)
         ringbuffer->enqueue(ringbuffer,&i);
     }
     
-    CuAssertIntEquals(tc, 4, ringbuffer->length);
+    int j;
+    for(int i = 0; i < 4; i++) {
+        ringbuffer->dequeue(ringbuffer,&j);
+        CuAssertIntEquals(tc, i, j);
+    }
+    
+    CuAssertIntEquals(tc, 0, ringbuffer->length);
     CuAssertIntEquals(tc, 5, ringbuffer->_capacity);
     CuAssertIntEquals(tc, sizeof(int), ringbuffer->_elemsize);
     CuAssertIntEquals(tc, 4, ringbuffer->_head);
-    CuAssertIntEquals(tc, 0, ringbuffer->_tail);
+    CuAssertIntEquals(tc, 4, ringbuffer->_tail);
     CuAssertPtrNotNull(tc,ringbuffer->_array);
-    CuAssertIntEquals(tc, 0, ringbuffer->isempty(ringbuffer));
-    CuAssertIntEquals(tc, 1, ringbuffer->isfull(ringbuffer));
+    CuAssertIntEquals(tc, 1, ringbuffer->isempty(ringbuffer));
+    CuAssertIntEquals(tc, 0, ringbuffer->isfull(ringbuffer));
 
     rb_delete(ringbuffer);
 }
---
+++ tests/rb-laparound-test.c
@@ -0,0 +1,40 @@
+#include "suite.h"
+#include "rb.h"
+
+void rb_create_test(CuTest * tc)
+{
+    struct rb * ringbuffer = rb_new(5000000,sizeof(int));
+    //*note the capacity is 5 but it can always only hold capacity-1
+    for(int i = 0; i < 4999999; i++) {
+        ringbuffer->enqueue(ringbuffer,&i);
+    }
+    
+    int j;
+    for(int i = 0; i < 4999999; i++) {
+        ringbuffer->dequeue(ringbuffer,&j);
+        CuAssertIntEquals(tc, i, j);
+    }
+    
+    for(int i = 0; i < 4999999; i++) {
+        ringbuffer->enqueue(ringbuffer,&i);
+    }
+
+    for(int i = 0; i < 4999999; i++) {
+        ringbuffer->dequeue(ringbuffer,&j);
+        CuAssertIntEquals(tc, i, j);
+    }
+
+    CuAssertIntEquals(tc, 0, ringbuffer->length);
+    CuAssertIntEquals(tc, 5000000, ringbuffer->_capacity);
+    CuAssertIntEquals(tc, sizeof(int), ringbuffer->_elemsize);
+    CuAssertIntEquals(tc, 4999998, ringbuffer->_head);
+    CuAssertIntEquals(tc, 4999998, ringbuffer->_tail);
+    CuAssertPtrNotNull(tc,ringbuffer->_array);
+    CuAssertIntEquals(tc, 1, ringbuffer->isempty(ringbuffer));
+    CuAssertIntEquals(tc, 0, ringbuffer->isfull(ringbuffer));
+
+    rb_delete(ringbuffer);
+}
+
+
+int main(void) { return execute(rb_create_test);}
