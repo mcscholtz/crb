a92d1f992aabc2e0691e8703cd317e4df5740671
Marius <mc.scholtz@gmail.com>
2018-09-01 13:45:21 -0700

    Added test for dequeue_a

---
+++ tests/rb-dequeuea-test.c
@@ -0,0 +1,32 @@
+#include "suite.h"
+#include "rb.h"
+
+void rb_dequeuea_test(CuTest * tc)
+{
+    struct rb * ringbuffer = rb_new(5,sizeof(int));
+    //*note the capacity is 5 but it can always only hold capacity-1
+    for(int i = 0; i < 4; i++) {
+        ringbuffer->enqueue(ringbuffer,&i);
+    }
+    
+    int * j;
+    for(int i = 0; i < 4; i++) {
+        j = ringbuffer->dequeue_a(ringbuffer);
+        CuAssertIntEquals(tc, i, *j);
+        free(j);
+    }
+    
+    CuAssertIntEquals(tc, 0, ringbuffer->length);
+    CuAssertIntEquals(tc, 5, ringbuffer->_capacity);
+    CuAssertIntEquals(tc, sizeof(int), ringbuffer->_elemsize);
+    CuAssertIntEquals(tc, 4, ringbuffer->_head);
+    CuAssertIntEquals(tc, 4, ringbuffer->_tail);
+    CuAssertPtrNotNull(tc,ringbuffer->_array);
+    CuAssertIntEquals(tc, 1, ringbuffer->isempty(ringbuffer));
+    CuAssertIntEquals(tc, 0, ringbuffer->isfull(ringbuffer));
+
+    rb_delete(ringbuffer);
+}
+
+
+int main(void) { return execute(rb_dequeuea_test);}
